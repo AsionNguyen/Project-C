ef44cf91da75b2b14d9e71a1ea8115370b70b2e4
AsionNguyen <41240653+AsionNguyen@users.noreply.github.com>
2021-01-26 19:14:26 +0100

    add comment: object

--- flint/test4.c
+++ flint/test4.c
@@ -1,3 +1,25 @@
+/* test object (poly, mat)  entry-manipulation
+   test factoring algo, thus finding integer roots
+   ----------------------------------------------------
+   used structures
+   - fmpz_poly_t{
+        fmpz* coeffs 
+        slong length
+        ...
+     }
+   - fmpz_mat_t{
+        fmpz **entry
+        ...
+     }
+   - fmpz_poly_factor_t{
+        slong num
+        fmpz_poly_t *p
+        slong exp
+        ...
+     }
+*/
+
+
 # include "fmpz_poly.h"
 # include "fmpz_lll.h"
 #include "fmpz_poly_factor.h"
@@ -10,12 +32,16 @@ int main(){
     slong i;
     int CHECK=1;
     
+    //Initialisation
     fmpz_poly_init(p);
     fmpz_mat_init(M,3,3);
     fmpz_poly_factor_init(fac);
     
+    //Setting
     fmpz_poly_set_str(p,"3  1 3 2");
     fmpz_mat_zero(M);
+    
+    //Entry manipulation
     *fmpz_mat_entry(M,0,0)=p->coeffs[0];
     //p->coeffs[0]=1;                               accessible
     fmpz_poly_print_pretty(p,s); printf("\n");
@@ -34,7 +60,7 @@ int main(){
     }
     if (CHECK==0) printf("No integer roots\n");
     
-
+    //Clearing
     fmpz_poly_clear(p);
     fmpz_mat_clear(M);
     fmpz_poly_factor_clear(fac);
